d34543462e0b38644057eda53f19dde4e549f616
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-06 21:00:02 -0800

    testing movieApi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-fetch('https://mdblist.p.rapidapi.com/?s=jaws', options)
+fetch('https://mdblist.com/api/?apikey=&mutnvco5tkvza4daour7p7zc4', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+		
