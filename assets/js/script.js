a5d270292a08acd9d80797bba6a6e16612d810dd
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-07 21:01:41 -0800

    Fixed Poster errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var button = document.querySelector("button");
 var inputEl = document.querySelector(".input-box");
 var bScoreEl = document.querySelector(".bScore");
 var mScoreEl = document.querySelector(".mScore");
+var moviePosterEl = document.querySelector(".movie")
 
 document.querySelector("button").addEventListener("click", function () {
 	var input = $(".input-box").val();
@@ -10,21 +11,26 @@ document.querySelector("button").addEventListener("click", function () {
 	fetch(movieApiTitle)
 		.then((response) => response.json())
 		.then((data) => {
-			var movieScore = data.search[0].search;
+			var movieScore = data.search[0].score;
 			var movieId = data.search[0].id;
 			console.log(movieId);
 			mScoreEl.textContent = movieScore / 10;
 			return(movieId);
 		})
-		.then(movieDescriptionGetter => {
-		console.log(movieDescriptionGetter);
+		.then(moviePosterGetter => {
+		console.log(moviePosterGetter);
 		
-		var movieDescription = "https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&i=" + movieDescriptionGetter;
-		fetch(movieDescription)
+		var moviePoster = "https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&i=" + moviePosterGetter;
+		fetch(moviePoster)
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data);
+				
+				console.log(data.poster);
+				var mPoster = data.poster;
+				var imageL = document.createElement("img");
+				imageL.setAttribute("src", mPoster);
+				document.querySelector(".movie-container").append(imageL);
 			});
 		});
-
+		
 });	
