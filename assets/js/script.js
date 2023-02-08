e28ce46c7074c3e54a9c37febf363be312bf00e5
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-07 19:07:29 -0800

    Fixed html errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,4 @@ fetch('https://mdblist.com/api/?apikey=&mutnvco5tkvza4daour7p7zc4', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-
-
-		
+	
