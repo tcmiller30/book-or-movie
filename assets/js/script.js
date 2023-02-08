396097ea32737a17313cac611b5ba899995db9cb
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-07 20:35:04 -0800

    Adding api to pull score and attatch to html div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
-fetch('https://mdblist.com/api/?apikey=&mutnvco5tkvza4daour7p7zc4', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+var button = document.querySelector("button");
+var inputEl = document.querySelector(".input-box");
+var bScoreEl = document.querySelector(".bScore");
+var mScoreEl = document.querySelector(".mScore");
+
+document.querySelector("button").addEventListener("click", function () {
+	var input = $(".input-box").val();
+	var movieApiTitle = "https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&s=" + input;
 	
+	fetch(movieApiTitle)
+		.then((response) => response.json())
+		.then((data) => {
+			var movieScore = data.search[0].search;
+			var movieId = data.search[0].id;
+			console.log(movieId);
+			mScoreEl.textContent = movieScore / 10;
+			return(movieId);
+		})
+		.then(movieDescriptionGetter => {
+		console.log(movieDescriptionGetter);
+		
+		var movieDescription = "https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&i=" + movieDescriptionGetter;
+		fetch(movieDescription)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+		});
+
+});	
