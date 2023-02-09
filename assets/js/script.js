ca9f5b3e004e22f992b2dd8674a66555677edaef
Travis <traviscmiller01@gmail.com>
2023-02-09 00:52:33 -0800

    Open Library APIs working, bookData obj returns as undefined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,89 @@
-// Global Variables
-var searchBar = document.getElementById('search');
-var submitBtn = document.getElementById('submit');
+var bookSearchUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings';
+var bookCoverEl = document.getElementById('book-cover')
 
 
-// Uses Open Library Search API to fetch the Work ID of a book for use in Open Library Works API
+var bookId;
+var bookTitle;
+var coverId;
+var rating;
 
-function bookSearch(){
-    var bookRequestUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
+var bookDataNull = {
+    bookTitle: '',
+    rating: ''
+};
 
-    fetch(bookRequestUrl)
-    .then(response => response.json())
-    .then(data => {
-       var bookKey = data.docs[0].key;
-       console.log(bookKey);
 
-    // 2nd API Fetch Request using the bookkey from fetch 1
-        // var workRequestUrl = 'https://openlibrary.org' + bookKey + '/ratings.json'
-        // fetch(workRequestUrl)
-        // .then(response => response.json())
-        // .then(data => {
-        //     var bookRating = data;
-        //     console.log(bookRating[0]);
-        // })
-       
+function bookAPIs(){
+    fetch(bookSearchUrl)
+    .then(function (response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        bookId = data.docs[0].key;
+        console.log(bookId);
+    // Once the bookId/key has been obtained by the first API, simultaneously init the fetch requests to the works and ratings API's
+    var worksRequestUrl = 'https://openlibrary.org' + bookId + '.json';
+    var ratingsRequestUrl = 'https://openlibrary.org' + bookId + '/ratings.json';
+    
+        return Promise.all([
+            fetch(worksRequestUrl),
+            fetch(ratingsRequestUrl)
+        ]).then(function (responses) {
+            // Get a JSON object from each of the responses
+            return Promise.all(responses.map(function (response) {
+                return response.json();
+            }));
+        }).then(function (data) {
+            // Log the data to the console
+            // You would do something with both sets of data here
+            // console.log data displays 2 json objects. Data[0] = Works API, Data[1] = Ratings API
+            console.log(data);
+            console.log(data[0]);
+            console.log(data[1]);
+            // create and return object for refence in other logic
+            
+                bookTitle = data[0].title;
+                coverId = data[0].covers[1];
+                rating = data[1].summary.average * 2;
+                        
+            console.log(bookTitle, coverId, rating)
+
+            //calls other 
+            bookDataObj();
+            bookCover();
+           
+           
+           
+           
+           
+            
+
+        })
+        
+
 
     })
 }
 
+function bookCover(){
+    bookCoverEl.src = 'https://covers.openlibrary.org/b/id/' + coverId + '-L.jpg'
+}
 
-bookSearch();
+function bookDataObj(){
+    var bookDataReturn = {
+        bookTitle : bookTitle,
+        rating: rating
+    };
+    
+    var bookData = Object.assign (bookDataNull, bookDataReturn);
+    console.log(bookData)
+    return bookData;
+    
+}
+bookAPIs();
+
+
+
+
+    
+    
