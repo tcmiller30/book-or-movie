bfdd9e8c53a3687d48f2404668b2a1b7ef44d328
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-09 19:02:04 -0800

    Adding movie poster and movie scoring feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,44 @@
 var button = document.querySelector("button");
 var inputEl = document.querySelector(".input-box");
+var bookCover = document.querySelector("#books");
+var summary = document.querySelector(".summary");
 var bScoreEl = document.querySelector(".bScore");
 var mScoreEl = document.querySelector(".mScore");
 var moviePosterEl = document.querySelector(".movie")
 
+
 document.querySelector("button").addEventListener("click", function () {
 	var input = $(".input-box").val();
-	var movieApiTitle = "https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&s=" + input;
-	
+	movieInfo(input);
+});
+
+function movieInfo(input) {
+	// var openBookApi = "https://openlibrary.org/search.json?q=" + input;
+	var movieApiTitle =
+		"https://mdblist.com/api/?apikey=0zlqcizpjwrma3fudekly1itt&s=" + input;
+
+	fetch(movieApiTitle)
+		.then((response) => response.json())
+		.then((data) => {
+			var movieData = data.search[0].imdbid;
+			console.log(movieData);
+
+			var imdbID = movieData;
+			var movieApiImdbId =
+				"https://infinite-cliffs-64050.herokuapp.com/https://mdblist.com/api?apikey=0zlqcizpjwrma3fudekly1itt&i=" +
+				imdbID;
+
+			fetch(movieApiImdbId)
+				.then((response) => response.json())
+				.then((data) => {
+					var movieSummary = data.description;
+					console.log(movieSummary);
+					summary.innerHTML = movieSummary;
+				});
+		});
+
+
+
 	fetch(movieApiTitle)
 		.then((response) => response.json())
 		.then((data) => {
@@ -32,5 +63,4 @@ document.querySelector("button").addEventListener("click", function () {
 				document.querySelector(".movie-container").append(imageL);
 			});
 		});
-		
-});	
+}
