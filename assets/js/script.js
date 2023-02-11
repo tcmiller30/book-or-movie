f8a6585a068be45ee459236611d7ce068152b039
Travis <traviscmiller01@gmail.com>
2023-02-11 14:18:59 -0800

    rearranged bScore and mScore to align image displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,7 @@ $(".input-box").on("keyup", function (e) {
 	if (e.keyCode === 13) {
 		var input = $(".input-box").val();
 		movieInfo(input);
-    bookAPIs(input);
+    	bookAPIs(input);
 	}
 });
 
